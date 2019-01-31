549b0a1420f47227dfb6fb446240a42c36501564
tinyduel <46302335+tinyduel@users.noreply.github.com>
2019-01-31 10:28:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,15 @@ function cashClick(number) {
   document.getElementById("cash").innerHTML = cash;
 };
 var keyboards = 0
+var keyboardCost = 10
 function buyKeyboards(){
-    var keyboardsCost = Math.floor(10 * Math.pow(1.1,keyboards));     //works out the cost of this cursor
-    if(cookies >= keyboardsCost){                                   //checks that the player can afford the cursor
-        keyboards = keyboards + 1;                                   //increases number of cursors
-    	  keyboards = keyboards - keyboardsCost;                          //removes the cookies spent
-        document.getElementById('keyboards').innerHTML = keyboards;  //updates the number of cursors for the user
-        document.getElementById('cash').innerHTML = cash;  //updates the number of cookies for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,keyboards));       //works out the cost of the next cursor
-    document.getElementById('keyboardsCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    if (cash >= keyboardCost)
+	    cash = cash - keyboardCost;
+	    keyboards = keyboards + 1
 };
 
 window.setInterval(function(){
 	
-	cookieClick(keyboards);
+	cashClick(keyboards);
 	
 }, 1000);
